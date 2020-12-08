4ee2fe49774fd0c155f2d90aa5f88aa86ca4a4ea
Tadataka-Matsumoto <tdtk1538.gmail.com>
2020-12-08 15:10:54 +0900

    ランダムウォークを追加

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,15 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+# x_values = [1, 2, 3, 4, 5]
+# y_values = [1, 4, 9, 16, 25]
+
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 #グラフのタイトルと軸ラベルを設定する
 ax.set_title("Square Numbers", fontsize=24)
@@ -15,5 +19,8 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 #目盛りラベルのサイズを設定する
 ax.tick_params(axis='both', which='major', labelsize=14)
+
+# 各軸の範囲を設定する
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
